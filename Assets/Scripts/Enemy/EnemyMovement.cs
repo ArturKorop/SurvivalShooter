166b6a2d3368b7436196d9f1c70cd8491b463c58
Artyr <rubkaarchi@gmail.com>
2015-03-23 21:07:19 +0200

    Enemy movement

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -3,26 +3,26 @@ using System.Collections;
 
 public class EnemyMovement : MonoBehaviour
 {
-    Transform player;
+    private Transform player;
     //PlayerHealth playerHealth;
     //EnemyHealth enemyHealth;
-    NavMeshAgent nav;
+    private NavMeshAgent nav;
 
 
     void Awake ()
     {
-        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        this.player = GameObject.FindGameObjectWithTag ("Player").transform;
         //playerHealth = player.GetComponent <PlayerHealth> ();
         //enemyHealth = GetComponent <EnemyHealth> ();
-        nav = GetComponent <NavMeshAgent> ();
+        this.nav = GetComponent <NavMeshAgent> ();
     }
-
+    
 
     void Update ()
     {
         //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
         //{
-            nav.SetDestination (player.position);
+            this.nav.SetDestination (player.position);
         //}
         //else
         //{
