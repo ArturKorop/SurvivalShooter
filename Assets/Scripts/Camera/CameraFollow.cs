2b869983553def5ed551549073baae445da63cf4
Artur Korop <rubkaarchi@gmail.com>
2015-03-23 16:31:28 +0200

    Camera

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public float Smoothing = 5f;
+
+    private Vector3 offset;
+
+    public void Start()
+    {
+        this.offset = this.transform.position - this.Target.position;
+    }
+
+    public void FixedUpdate()
+    {
+        var targetCamPos = this.Target.position + offset;
+        this.transform.position = Vector3.Lerp(transform.position, targetCamPos, this.Smoothing * Time.deltaTime);
+    }
+}
