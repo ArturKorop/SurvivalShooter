d7a1d1b9a25caca17a1ad48043fd779c733da7ef
Artur Korop <rubkaarchi@gmail.com>
2015-03-25 11:34:07 +0200

    Update harming enemy

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,29 +4,29 @@ using System.Collections;
 public class EnemyMovement : MonoBehaviour
 {
     private Transform player;
-    //PlayerHealth playerHealth;
-    //EnemyHealth enemyHealth;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
     private NavMeshAgent nav;
 
 
     void Awake ()
     {
         this.player = GameObject.FindGameObjectWithTag ("Player").transform;
-        //playerHealth = player.GetComponent <PlayerHealth> ();
-        //enemyHealth = GetComponent <EnemyHealth> ();
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
         this.nav = GetComponent <NavMeshAgent> ();
     }
     
 
     void Update ()
     {
-        //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-        //{
-            this.nav.SetDestination (player.position);
-        //}
-        //else
-        //{
-        //    nav.enabled = false;
-        //}
+        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            this.nav.SetDestination(player.position);
+        }
+        else
+        {
+            nav.enabled = false;
+        }
     }
 }
